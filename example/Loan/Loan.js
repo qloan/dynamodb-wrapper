1b3bbfe1b529fb3252df8b157d8a763dc5af4e8a
danielsmithmichigan <dansmith@quickenloans.com>
2016-09-29 11:48:55 -0400

    [proposed-table-structure] WIP

--- example/Loan/Loan.js
+++ example/Loan/Loan.js
@@ -1,11 +1,17 @@
 var Item = require("../../lib/item");
 var joi = require('joi');
 var async = require('async');
+/* Hooks would get defined in this file I think */
 class Loan extends Item {
     constructor(json) {
         super({
             attrs: json,
-            model: require("./LoanTable")
+            model: require("./TableDefinition"),
+            itemConstructor: Loan
         });
+    }
+    // Extensions are just functions
+    getFoo(foo) {
+        return 'Hi, ' + "foo" + '!';
     }
 }
---
+++ example/Loan/TableDefinition.js
@@ -0,0 +1,14 @@
+var DB = require('./lib')({
+    region: 'us-west-2'
+});
+var dbObject = new DB({
+    tableName: 'dev-dsmith11-loans',
+    key: {
+        hash: 'loanId'
+    },
+    schema: require("./schema")
+});
+
+module.exports = function() {
+    return dbObject;
+};
