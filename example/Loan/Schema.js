8d345fa6617c84375eddd434bc8f640eac83417c
turntayble81 <turntayble81@gmail.com>
2016-09-30 12:59:48 -0400

    Exposed JoiCrypt instance in module. Updated example.

--- example/Loan/Schema.js
+++ example/Loan/Schema.js
@@ -1,17 +1,22 @@
-var joi = require("joi");
-var Schema = require('../../lib').schema({
+const DB       = require('../../lib');
+let joi        = DB.joi;
+let LoanSchema = DB.schema({
     region: 'us-west-2'
 });
 
 /* Hooks which operate on the plain json of the items would be defined in this file */
 
-module.exports = new Schema({
+module.exports = new LoanSchema({
     tableName: 'dev-dsmith11-loans',
     key: {
         hash: 'loanId'
     },
     schema: {
         loanId : joi.string().required(),
+        personalInformation : joi.object().keys({
+            firstName : joi.string().encrypt(),
+            lastName  : joi.string().encrypt()
+        }),
         foo    : joi.string().required()
     }
 });
