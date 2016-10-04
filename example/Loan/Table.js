677a7d55a71279050648d834a5369febc9afb8eb
danielsmithmichigan <dansmith@quickenloans.com>
2016-10-04 14:20:40 -0400

    Changed some dependencies

--- example/Loan/Table.js
+++ example/Loan/Table.js
@@ -2,6 +2,6 @@ var Table = require("../../lib").table;
 
 /* require this file if you need get, scan, or query */
 module.exports = new Table({
-    schema          : require("./Schema"),
-    itemConstructor : require("./Item")
+    schema          : require("./schema"),
+    itemConstructor : require("./item")
 });
