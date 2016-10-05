9f5885dd8b6a696bbb8d7ff10aadab293bd6273f
danielsmithmichigan <dansmith@quickenloans.com>
2016-10-05 15:56:05 -0400

    [item-extensions] Added extensions and tests for that

---
+++ example/loan/extensions/bankAccount.js
@@ -0,0 +1,22 @@
+function create() {
+
+}
+
+function update() {
+
+}
+
+function del() {
+
+}
+
+function read() {
+
+}
+
+module.exports = {
+    create: create,
+    update: update,
+    del: del,
+    read: read
+};
